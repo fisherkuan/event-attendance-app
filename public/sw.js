935396877044d5e4fe945ea59ffe5e6c1363fc6a
Fisher Kuan <kuanfisher@gmail.com>
2025-09-26 18:03:09 +0200

    clear cache automatically

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = 'event-attendance-app-cache-v1';
+const CACHE_NAME = 'event-attendance-app-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -20,14 +19,31 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
   );
 });
